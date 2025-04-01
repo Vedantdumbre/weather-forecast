6dc084234eafa006a4e0ec0042ae3ed7015422be
Vedant <vedantdumbre0706@gmail.com>
2025-04-01 23:47:42 +0530

    setting up api

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/api', (req, res) => {
+    res.send('Weather Forecast API');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
